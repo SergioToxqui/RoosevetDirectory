cc53f047098a3b0ea2dd7dbe79c5612fce180f62
Serg <sergio.toxqui@gmail.com>
2018-05-05 13:57:18 -0400

    setting up the navigation pages

---
+++ front-end/src/Navigation/Categories.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+
+
+const Categories = () => {
+    return (
+      <div className="categories">
+      <div className="food">FOOD</div>
+      <div className="stores">STORES</div>
+      <div className="services">SERVICES</div>
+      <div className="nightlife">NIGHTLIFE</div>
+      </div>
+    );
+  };
+  
+  export default Categories;
