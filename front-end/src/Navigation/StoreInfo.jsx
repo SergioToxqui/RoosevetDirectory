4ebafa60883226e136ab0d5f407e357316d36f9e
Serg <sergio.toxqui@gmail.com>
2018-07-08 18:16:55 -0400

    fixed the iframe issue again

--- front-end/src/Navigation/StoreInfo.jsx
+++ front-end/src/Navigation/StoreInfo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import Iframe from "../src/react-iframe/index";
+import Iframe from "../react-iframe/index";
 import { Carousel } from "react-responsive-carousel";
 import styles from 'react-responsive-carousel/lib/styles/carousel.min.css'
 
