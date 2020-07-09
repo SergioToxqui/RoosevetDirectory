3ec9e9410f1638a36419f2788446e4cfc720d03c
alejo4373 <alejandrofranco@pursuit.org>
2020-07-09 17:11:02 -0400

    add dotenv and use environment variables for database url and port

--- back-end/app.js
+++ back-end/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -29,14 +30,14 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '../front-end/build/index.html'));
 });
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
