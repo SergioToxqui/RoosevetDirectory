b9eb4be79285401a09c23cbb83148cc76148b706
alejo4373 <alejandrofranco@pursuit.org>
2021-05-15 22:03:28 -0400

    Aggregate pictures in a JSON array of objects

--- back-end/db/Businesses.js
+++ back-end/db/Businesses.js
@@ -53,10 +53,13 @@ const getBySlug = async (slug) => {
     const SQL = `
       SELECT
         businesses.id, name, name_slug, phone, address_1,
-        address_2, description, active, status, thumb_img
+        address_2, description, active, status, thumb_img,
+        json_agg(json_build_object('url', objects.url, 'type', objects.type)) as objects
       FROM businesses
-      WHERE name_slug = $1
+      JOIN objects on businesses.id = objects.business_id
+      WHERE name_slug = $1 AND objects.type::text LIKE '%photo'
       AND active = TRUE
+      GROUP BY businesses.id
     `
     const data = await db.oneOrNone(SQL, slug)
     return data
