8eb15e497197f5fae77c6833ba0158bc2ac46be7
alejo4373 <alejandrofranco@pursuit.org>
2021-05-29 21:31:09 -0400

    Display formatted address and fix embeded map

--- front-end/src/Navigation/BusinessPage.jsx
+++ front-end/src/Navigation/BusinessPage.jsx
@@ -57,6 +57,7 @@ class BusinessPage extends React.Component {
 
   render() {
     const { biz, schedule, categories } = this.state
+    const formattedAddress = `${biz.address_1} ${biz.address_2 ? ", " + biz.address_2 : ""}`
     if (!biz || !biz.id) return <p>Loading...</p>
     let items = biz.objects.map((obj, i) => {
       return (
@@ -71,71 +72,76 @@ class BusinessPage extends React.Component {
       )
     })
     return (
-      <div className="ui grid container">
-        <div className="column eight wide">
-          <h1 className="ui header">{biz.name}</h1>
-          <div className="slider-wrapper">
-            <Slider dots={true}>{items}</Slider>
-          </div>
-          <div className="storeInfoMaps">
-            <Iframe
-              url={`https://maps.google.com/maps?q=${biz.address}&output=embed`}
-              id="myId"
-              className="myClassname"
-              display="initial"
-              position="relative"
-              width="100%"
-              height="100%"
-              allowFullScreen
-            />
-          </div>
-        </div>
-        <div className="eight wide column">
+      <div>
 
-          <div className="ui segment basic">
-            <h3 className="ui header">Description:</h3>
-            <p> {biz.description}</p>
-          </div>
-
-          <div className="ui segment basic">
-            <h3>Address:</h3>
-            <p> {biz.address_1}</p>
+        <h1 className="ui header">{biz.name}</h1>
+        <div className="ui grid container">
+          <div className="column eight wide">
+            <div className="ui segment vertical basic">
+              <h3>Gallery</h3>
+              <div className="slider-wrapper">
+                <Slider dots={true}>{items}</Slider>
+              </div>
+            </div>
+            <div className="ui segment vertical basic">
+              <h3>Address & Location</h3>
+              <p> {formattedAddress}</p>
+              <div className="storeInfoMaps">
+                <Iframe
+                  url={`https://maps.google.com/maps?q=${formattedAddress}&output=embed`}
+                  id="myId"
+                  className="myClassname"
+                  display="initial"
+                  position="relative"
+                  width="100%"
+                  height="100%"
+                  allowFullScreen
+                />
+              </div>
+            </div>
           </div>
 
-          <div className="ui segment basic compact">
-            <h3>Schedule:</h3>
-            <table class="ui striped compact table">
-              <tbody>
-                {schedule.map(day => (
-                  <tr key={day.day} >
-                    <td className="dow">{day.day}</td>
-                    <td>{day.open_time + " - " + day.close_time}</td>
-                    {/* <td>{day.close_time}</td> */}
-                  </tr>
+          <div className="eight wide column">
+            <div className="ui segment vertical basic">
+              <h3 className="ui header">Description:</h3>
+              <p> {biz.description}</p>
+            </div>
+
+            <div className="ui segment vertical basic compact">
+              <h3>Schedule:</h3>
+              <table class="ui striped compact table">
+                <tbody>
+                  {schedule.map(day => (
+                    <tr key={day.day} >
+                      <td className="dow">{day.day}</td>
+                      <td>{day.open_time + " - " + day.close_time}</td>
+                      {/* <td>{day.close_time}</td> */}
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <p> {biz.address}</p>
+            </div>
+
+            <div className="ui segment vertical basic">
+              <h3>Categories:</h3>
+              <div className="ui tag labels">
+                {categories.map(category => (
+                  <span className="ui label"> {category.name}</span>
                 ))}
-              </tbody>
-            </table>
-            <p> {biz.address}</p>
-          </div>
+              </div>
+            </div>
 
-          <div className="ui segment basic">
-            <h3>Categories:</h3>
-            <div className="ui labels">
-              {categories.map(category => (
-                <span className="ui tag label"> {category.name}</span>
-              ))}
+            <div className="ui segment vertical basic">
+              <h3>Keywords:</h3>
+              <div className="ui circular labels">
+                {biz.keywords.split(' ').map(keyword => (
+                  <span className="ui label"> {keyword}</span>
+                ))}
+              </div>
             </div>
           </div>
-
-          <div className="ui segment basic">
-            <h3>Keywords:</h3>
-            <div className="ui circular labels">
-              {biz.keywords.split(' ').map(keyword => (
-                <span className="ui label"> {keyword}</span>
-              ))}
-            </div>
-          </div>
-        </div>
+        </div >
       </div >
     );
   }
