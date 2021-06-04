db78a8e5f604c5d08715f0f9b4e4b0c5c02ac6c7
alejo4373 <alejandrofranco@pursuit.org>
2021-06-04 02:05:17 -0400

    Make top menu search bar functional

--- front-end/src/Navigation/TopMenu.jsx
+++ front-end/src/Navigation/TopMenu.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
 import "../Stylesheets/TopMenu.css";
 
 const TopMenu = () => {
+  const [searchTerms, setSearchTerms] = useState('')
+  const history = useHistory()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    history.push(`/search?q=${searchTerms}`)
+  }
+
   return (
     <div className="top-menu ui menu stackable borderless">
       <div className="ui two column stackable grid container" style={{ margin: 0 }}>
@@ -10,14 +19,21 @@ const TopMenu = () => {
           <Link to='/'><img src="https://image.ibb.co/n3Qkin/logo50.png" alt=" roosevelt directory logo" /></Link>
           <p className="top-menu__title"> | Roosevelt Directory</p>
         </div>
-        <div className="ui item ten wide column">
+        <form onSubmit={handleSubmit} className="ui item ten wide column">
           <div className="ui input">
-            <input type="text" placeholder="Search for tacos, hardware store, clothing, etc" />
+            <input
+              type="text"
+              placeholder="Search for tacos, hardware store, clothing, etc"
+              onChange={(e) => setSearchTerms(e.target.value)}
+              value={searchTerms}
+            />
           </div>
-          <Link className="top-menu__search-icon" to='/'><img src=" https://image.ibb.co/fxtQE8/search_copy.png" alt="search" /></Link>
-        </div>
+          <button className="top-menu__search-icon">
+            <img src=" https://image.ibb.co/fxtQE8/search_copy.png" alt="search" />
+          </button>
+        </form>
       </div>
-    </div >
+    </div>
   );
 };
 
