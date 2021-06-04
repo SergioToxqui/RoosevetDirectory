946091f4f42e30f2944d289bb313f08cae9dd67d
alejo4373 <alejandrofranco@pursuit.org>
2021-06-04 00:23:10 -0400

    Remove unused css

--- front-end/src/Navigation/BusinessCategories.jsx
+++ front-end/src/Navigation/BusinessCategories.jsx
@@ -29,9 +29,11 @@ const BusinessCategories = (props) => {
   return (
     <div>
       <div className="category-menu">
-        <div className="category-menu__title" style={{ backgroundImage: `url(${categoryData.image_url})` }}>
-          <Link to={`/${categoryData.name}`}>{categoryData.name.toUpperCase()}</Link>
-        </div>
+        <Link to={`/${categoryData.name}`}>
+          <div className="category-menu__title" style={{ backgroundImage: `url(${categoryData.image_url})` }}>
+            {categoryData.name.toUpperCase()}
+          </div>
+        </Link>
         <div className="category-menu__description">{categoryData.description}</div>
       </div>
       <Switch>
